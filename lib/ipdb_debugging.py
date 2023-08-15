1bd7cf9787fad295ae4531b6ee4945ff6c4a305f
michellemwangi01 <michellemwangim@gmail.com>
2023-08-15 10:04:08 +0300

    ipdb completed

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -1,7 +1,17 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 
 import ipdb
 
 def plus_two(num):
-    num + 2
+    num += 2
+    # ipdb.set_trace()
     return num
+
+
+# my_list = [12, 12]
+# def get_sum(n):
+#      # for i in range(len(n)):
+#     sum = (n*(n+1)/2)
+#     return sum
+#
+# print(get_sum(len(my_list)))
